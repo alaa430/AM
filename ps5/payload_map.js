9f6c1c6444266b19823c92fdf4ca40ed665bb51e
alaa430 <llballba19@gmail.com>
2024-12-12 21:51:45 +0200

    Add files via upload

--- ps5/payload_map.js
+++ ps5/payload_map.js
@@ -19,156 +19,138 @@ const CUSTOM_ACTION_APPCACHE_REMOVE = "appcache-remove";
 /**
  * @type {PayloadInfo[]}
 */
-const payload_map =
-    [
-        {
-            displayTitle: 'etaHEN',
-            description: 'All-in-one HEN',
-            fileName: 'etaHEN-1.8B.bin',
-            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
-            source: 'https://github.com/LightningMods/etaHEN',
-            version: '1.8 beta'
-        },
-        {
-            displayTitle: 'K-Stuff',
-            description: 'FPKG enabler',
-            fileName: 'ps5-kstuff-v1.4.bin',
-            author: 'sleirsgoevy',
-            source: 'https://github.com/idlesauce/ps5-kstuff-builds/releases',
-            version: '1.4'
-        },
-        {
-            displayTitle: 'libhijacker game-patch',
-            description: 'Patches supported games to run at higher framerates, and adds debug menus to certain titles.',
-            fileName: 'libhijacker-1.160.elf',
-            author: 'illusion0001, astrelsky',
-            source: 'https://github.com/illusion0001/libhijacker/releases',
-            version: '1.160'
-        },
-        {
-            displayTitle: 'ps5debug',
-            description: 'Debugger (Experimental beta)',
-            fileName: 'ps5debug.elf',
-            author: 'SiSTR0, ctn123',
-            source: 'https://github.com/GoldHEN/ps5debug',
-            version: '1.0b1'
-        },
-        {
-            displayTitle: 'HEN-V',
-            description: 'PS5 Homebrew Enabler, FTP, klog',
-            fileName: 'henv.elf',
-            author: 'astrelsky',
-            source:'https://github.com/astrelsky/HEN-V/releases',
-            version: '0.0.2-alpha'
-        },
-        {
-            displayTitle: 'PS5 Payload ELF Loader',
-            description: 'Uses port 9021. Persistent network elf loader',
-            fileName: 'elfldr.elf',
-            author: 'john-tornblom',
-            source:'https://github.com/ps5-payload-dev/elfldr/releases',
-            version: '0.14'
-        },
-        {
-            displayTitle: 'websrv',
-            description: "Uses john-tornblom's elfldr. Custom homebrew loader. Runs on port 8080.",
-            fileName: 'websrv.elf',
-            author: 'john-tornblom',
-            loader: 'john-tornblom-elfldr',
-            source:'https://github.com/ps5-payload-dev/websrv/releases',
-            version: '0.8'
-        },
-        {
-            displayTitle: 'shsrv',
-            description: "Uses john-tornblom's elfldr. Telnet shell server. Runs on port 2323.",
-            fileName: 'shsrv.elf',
-            author: 'john-tornblom',
-            loader: 'john-tornblom-elfldr',
-            source:'https://github.com/ps5-payload-dev/shsrv/releases',
-            version: '0.10'
-        },
-        {
-            displayTitle: 'ftpsrv',
-            description: "Uses john-tornblom's elfldr. FTP server. Runs on port 2121.",
-            fileName: 'ftpsrv.elf',
-            author: 'john-tornblom',
-            loader: 'john-tornblom-elfldr',
-            source:'https://github.com/ps5-payload-dev/ftpsrv/releases',
-            version: '0.10'
-        },
-        {
-            displayTitle: 'klogsrv',
-            description: "Uses john-tornblom's elfldr. Klog server. Runs on port 3232.",
-            fileName: 'klogsrv.elf',
-            author: 'john-tornblom',
-            loader: 'john-tornblom-elfldr',
-            source:'https://github.com/ps5-payload-dev/klogsrv/releases',
-            version: '0.4'
-        },
-        {
-            displayTitle: 'gdbsrv',
-            description: "Uses john-tornblom's elfldr. GDB server. Runs on port 2159.",
-            fileName: 'gdbsrv.elf',
-            author: 'john-tornblom',
-            loader: 'john-tornblom-elfldr',
-            source:'https://github.com/ps5-payload-dev/gdbsrv/releases',
-            version: '0.3'
-        },
-        {
-            displayTitle: 'FTPS5 (Non-Persistent)',
-            description: 'FTP Server',
-            fileName: 'ftps5-np.elf',
-            author: 'SiSTR0, zecoxao, EchoStretch',
-            source:'https://github.com/EchoStretch/FTPS5/releases',
-            version: '1.4'
-        },
-        {
-            displayTitle: 'FTPS5 (Persistent)',
-            description: 'FTP Server, causes kernel panic on shutdown.',
-            fileName: 'ftps5-p.elf',
-            author: 'SiSTR0, zecoxao, EchoStretch',
-            source:'https://github.com/EchoStretch/FTPS5/releases',
-            version: '1.4'
-        },
-        {
-            displayTitle: 'Versions',
-            description: 'Shows kernel build, os and sdk versions',
-            fileName: 'versions.elf',
-            author: 'SiSTRo',
-            source:'https://github.com/SiSTR0/ps5-versions/releases/download/v1.0/ps5-versions.elf',
-            version: '1.0'
-        },
-        {
-            displayTitle: 'GetOSVersion',
-            description: 'very slow',
-            fileName: 'getOsVersion.elf',
-            author: 'logic-68',
-            source:'https://github.com/logic-68/getOsVersion/releases',
-            version: '1.0.2'
-        },
-        {
-            displayTitle: 'PS5_Browser_appCache_remove',
-            description: 'Removes appcache from browser.',
-            fileName: 'Browser_appCache_remove.elf',
-            author: 'Storm21CH',
-            source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
-            version: '1.0fix'
-        },
-        {
-            displayTitle: 'PS5-BackupDB',
-            description: 'Backups up main databases to usb.',
-            fileName: 'BackupDB.elf',
-            author: 'Storm21CH, Jeroendev, Logic68',
-            source:'https://github.com/Storm21CH/PS5-BackupDB/releases/tag/v1.00',
-            version: '1.00'
-        },
-        {
-            displayTitle: 'PS5-BackupDbUser',
-            description: 'May take a long time! Backups up main databases and user files to usb.',
-            fileName: 'BackupDbUser.elf',
-            author: 'Storm21CH, Jeroendev, Logic68',
-            source:'https://github.com/Storm21CH/PS5-BackupDbUser/releases/tag/v1.00',
-            version: '1.00'
-        }
+const payload_map = [
+    // { // auto-loaded
+    //     displayTitle: "PS5 Payload ELF Loader",
+    //     description: "Uses port 9021. Persistent network elf loader",
+    //     fileName: "elfldr.elf",
+    //     author: "john-tornblom",
+    //     projectSource: "https://github.com/ps5-payload-dev/elfldr",
+    //     binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+    //     version: "?",
+    //     supportedFirmwares: ["1.", "2.", "3.", "4.", "5."]
+    // },
+    {
+        displayTitle: "ps5-kstuff",
+        description: "FPKG enabler",
+        fileName: "ps5-kstuff.bin",
+        author: "sleirsgoevy",
+        projectSource: "https://github.com/sleirsgoevy/ps4jb-payloads/tree/bd-jb/ps5-kstuff",
+        binarySource: "https://github.com/sleirsgoevy/ps4jb2/blob/3e6053c3e4c691a9ccdc409172293a81de00ad7f/ps5-kstuff.bin",
+        version: "3e6053c",
+        supportedFirmwares: ["3.", "4."]
+    },
+    {
+        displayTitle: "Byepervisor HEN",
+        description: "FPKG enabler",
+        fileName: "byepervisor.elf",
+        author: "SpecterDev, ChendoChap, flatz, fail0verflow, Znullptr, kiwidog, sleirsgoevy, EchoStretch, LightningMods, BestPig, zecoxao", 
+        projectSource: "https://github.com/EchoStretch/Byepervisor",
+        binarySource: "https://github.com/EchoStretch/Byepervisor/actions/runs/11946215784",
+        version: "5a0d933",
+        supportedFirmwares: ["1.00", "1.01", "1.02", "1.05", "1.12", "1.14", "2.00", "2.20", "2.25", "2.26", "2.30", "2.50", "2.70"],
+        toPort: 9021
+    },
+    {
+        displayTitle: "libhijacker game-patch",
+        description: "Patches supported games to run at higher framerates, and adds debug menus to certain titles.",
+        fileName: "libhijacker-game-patch.v1.160.elf",
+        author: "illusion0001, astrelsky",
+        projectSource: "https://github.com/illusion0001/libhijacker",
+        binarySource: "https://github.com/illusion0001/libhijacker-game-patch/releases/tag/1.160-75ab26a3",
+        version: "1.160",
+        supportedFirmwares: ["3.", "4."]
+    },
+    {
+        displayTitle: "websrv",
+        description: "Uses john-tornblom's elfldr. Custom homebrew loader. Runs on port 8080.",
+        fileName: "websrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/websrv/releases",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+        version: "0.14",
+        toPort: 9021
+    },
+    {
+        displayTitle: "ftpsrv",
+        description: "Uses john-tornblom's elfldr. FTP server. Runs on port 2121.",
+        fileName: "ftpsrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/ftpsrv",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+        version: "0.11",
+        toPort: 9021
+    },
+    {
+        displayTitle: "klogsrv",
+        description: "Uses john-tornblom's elfldr. Klog server. Runs on port 3232.",
+        fileName: "klogsrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/klogsrv/releases",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+        version: "0.5",
+        toPort: 9021
+    },
+    {
+        displayTitle: "shsrv",
+        description: "Uses john-tornblom's elfldr. Telnet shell server. Runs on port 2323.",
+        fileName: "shsrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/shsrv/releases",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+        version: "0.12",
+        toPort: 9021
+    },
+    {
+        displayTitle: "gdbsrv",
+        description: "Uses john-tornblom's elfldr. GDB server. Runs on port 2159.",
+        fileName: "gdbsrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/gdbsrv/releases",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+        version: "0.4-1",
+        toPort: 9021
+    },
+    {
+        displayTitle: "ps5debug",
+        description: "Debugger (Experimental beta)",
+        fileName: "ps5debug.elf",
+        author: "SiSTR0, ctn123",
+        projectSource: "https://github.com/GoldHEN/ps5debug",
+        binarySource: "https://github.com/GoldHEN/ps5debug/releases/download/1.0b1/ps5debug_v1.0b1.7z",
+        version: "1.0b1",
+        supportedFirmwares: ["3.", "4."]
+    },
+    {
+        displayTitle: "ps5debug",
+        description: "Debugger, open source version by DizzRL",
+        fileName: "ps5debug_dizz.elf",
+        author: "Dizz, astrelsky, John Tornblom, SiSTR0, golden, idlesauce",
+        projectSource: "https://github.com/idlesauce/ps5debug",
+        binarySource: "https://github.com/idlesauce/ps5debug/releases/download/v0.0.1/ps5debug.elf",
+        version: "0.0.1",
+        toPort: 9021
+    },
+    {
+        displayTitle: "ps5-versions",
+        description: "Shows kernel build, os and sdk versions",
+        fileName: "ps5-versions.elf",
+        author: "SiSTRo",
+        projectSource: "https://github.com/SiSTR0/ps5-versions",
+        binarySource: "https://github.com/SiSTR0/ps5-versions/releases/download/v1.0/ps5-versions.elf",
+        version: "1.0",
+        supportedFirmwares: ["1.", "2.", "3.", "4."]
+    },
+    {
+        // https://github.com/Storm21CH/PS5_Browser_appCache_remove
+        displayTitle: "Browser appcache remover",
+        description: "Deletes for only the current user in webkit-only mode",
+        fileName: "",
+        author: "Storm21CH, idlesauce",
+        projectSource: "",
+        binarySource: "",
+        version: "1.0",
+        customAction: CUSTOM_ACTION_APPCACHE_REMOVE
+    }
 
-    ];
+];
