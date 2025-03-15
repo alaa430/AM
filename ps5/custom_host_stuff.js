1a5a5d578e47790acc4733587e92577a3fef4158
alaa430 <llballba19@gmail.com>
2025-03-15 16:00:05 +0200

    Add files via upload

--- ps5/custom_host_stuff.js
+++ ps5/custom_host_stuff.js
@@ -2,13 +2,12 @@ const LOCALSTORE_WK_EXPLOIT_TYPE_KEY = "wk_exploit_type";
 const LOCALSTORE_WK_EXPLOIT_TYPE_VALUE_PSFREE = "PSFree";
 const LOCALSTORE_WK_EXPLOIT_TYPE_VALUE_FONTFACE = "FontFace";
 
-const SESSIONSTORE_ON_LOAD_AUTORUN_KEY = "on_load_autorun";
+const LOCALSTORE_REDIRECTOR_LAST_URL_KEY = "redirector_last_url";
 
-const MAINLOOP_EXECUTE_PAYLOAD_REQUEST = "mainloop_execute_payload_request";
+const SESSIONSTORE_RUN_WK_EXPLOIT_ON_LOAD_KEY = "run_wk_exploit_on_load";
 
 let exploitStarted = false;
-
-async function run(wkonly = false, animate = true) {
+async function runJailbreak(animate = true) {
     if (exploitStarted) {
         return;
     }
@@ -16,38 +15,34 @@ async function run(wkonly = false, animate = true) {
 
     await switchPage("console-view", animate);
 
-    sessionStorage.setItem(SESSIONSTORE_ON_LOAD_AUTORUN_KEY, wkonly ? "wkonly" : "kernel");
+    // not setting it in the catch since we want to retry both on a handled error and on a browser crash
+    sessionStorage.setItem(SESSIONSTORE_RUN_WK_EXPLOIT_ON_LOAD_KEY, "true");
 
     let wk_exploit_type = localStorage.getItem(LOCALSTORE_WK_EXPLOIT_TYPE_KEY);
     try {
         if (!animate) {
+            // hack but waiting a bit seems to help
+            // this only gets hit when auto-running on page load
             await new Promise((resolve) => setTimeout(resolve, 100));
         }
         if (wk_exploit_type == LOCALSTORE_WK_EXPLOIT_TYPE_VALUE_PSFREE) {
             debug_log("[+] running psfree for userland exploit...");
-            await run_psfree(fw_str);
+            await run_psfree();
         } else if (wk_exploit_type == LOCALSTORE_WK_EXPLOIT_TYPE_VALUE_FONTFACE) {
             debug_log("[+] running fontface for userland exploit...");
             await run_fontface();
         }
     } catch (error) {
         debug_log("[!] Webkit exploit failed: " + error);
-
         debug_log("[+] Retrying in 2 seconds...");
+
         await new Promise((resolve) => setTimeout(resolve, 2000));
         window.location.reload();
-        return;
+        return; // this is necessary
     }
 
-    try {
-        await main(wkonly);
-    } catch (error)  {
-        debug_log("[!] Kernel exploit/main() failed: " + error);
-    }
-
-    debug_log("[+] Retrying in 4 seconds...");
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-    window.location.reload();
+    sessionStorage.removeItem(SESSIONSTORE_RUN_WK_EXPLOIT_ON_LOAD_KEY);
+    run_hax();
 }
 
 async function switchPage(id, animate = true) {
@@ -63,6 +58,7 @@ async function switchPage(id, animate = true) {
         if (animate) {
             let oldSelectedElementTransitionEnd = new Promise((resolve) => {
                 oldSelectedElement.addEventListener("transitionend", function handler(event) {
+                    // we get back transitionend for children too but we don't want that
                     if (event.target === oldSelectedElement) {
                         oldSelectedElement.removeEventListener("transitionend", handler);
                         resolve();
@@ -72,6 +68,7 @@ async function switchPage(id, animate = true) {
             oldSelectedElement.classList.remove('selected');
             await oldSelectedElementTransitionEnd;
         } else {
+            // override transition with none for instant switch
             oldSelectedElement.style.setProperty('transition', 'none', 'important');
             oldSelectedElement.offsetHeight;
             oldSelectedElement.classList.remove('selected');
@@ -83,6 +80,7 @@ async function switchPage(id, animate = true) {
     if (animate) {
         let targetElementTransitionEnd = new Promise((resolve) => {
             targetElement.addEventListener("transitionend", function handler(event) {
+                // we get back transitionend for children too but we don't want that
                 if (event.target === targetElement) {
                     targetElement.removeEventListener("transitionend", handler);
                     resolve();
@@ -92,6 +90,7 @@ async function switchPage(id, animate = true) {
         targetElement.classList.add('selected');
         await targetElementTransitionEnd;
     } else {
+        // override transition with none for instant switch
         targetElement.style.setProperty('transition', 'none', 'important');
         targetElement.offsetHeight;
         targetElement.classList.add('selected');
@@ -100,9 +99,6 @@ async function switchPage(id, animate = true) {
     }
 }
 
-window.onload = async function() {
-    await run();
-};
 
 function registerAppCacheEventHandlers() {
     var appCache = window.applicationCache;
@@ -126,59 +122,77 @@ function registerAppCacheEventHandlers() {
 
     if (document.documentElement.hasAttribute("manifest")) {
         if (!navigator.onLine) {
-            createOrUpdateAppCacheToast('★ Off-line wait...', 2000);
+            createOrUpdateAppCacheToast('Offline.', 2000);
         } else {
-            createOrUpdateAppCacheToast("★ Check updates...");
-        }
+            // this is redundant
+            createOrUpdateAppCacheToast("Checking for updates...");
+        }        
     }
 
     appCache.addEventListener('cached', function (e) {
-        createOrUpdateAppCacheToast('★ Finished caching site', 1500);
+        createOrUpdateAppCacheToast('Finished caching site.', 1500);
     }, false);
 
     appCache.addEventListener('checking', function (e) {
-        createOrUpdateAppCacheToast('★ Check updates...');
+        createOrUpdateAppCacheToast('Checking for updates...');
     }, false);
 
     appCache.addEventListener('downloading', function (e) {
-        createOrUpdateAppCacheToast('★ Downloading cache');
+        createOrUpdateAppCacheToast('Downloading new cache...');
     }, false);
 
     appCache.addEventListener('error', function (e) {
+        // only show error toast if we're online
         if (navigator.onLine) {
-            createOrUpdateAppCacheToast('★ Error caching ', 5000);
+            createOrUpdateAppCacheToast('Error while caching site.', 5000);
         } else {
-            createOrUpdateAppCacheToast('★ Off-line wait...', 2000);
+            createOrUpdateAppCacheToast('Offline.', 2000);
         }
     }, false);
 
     appCache.addEventListener('noupdate', function (e) {
-        createOrUpdateAppCacheToast('★ Cache is up', 1500);
+        createOrUpdateAppCacheToast('Cache is up-to-date.', 1500);
     }, false);
 
     appCache.addEventListener('obsolete', function (e) {
-        createOrUpdateAppCacheToast('★ Site is obsolete.');
+        createOrUpdateAppCacheToast('Site is obsolete.');
     }, false);
 
     appCache.addEventListener('progress', function (e) {
-    let dots = '.'.repeat(Math.min(Math.floor((e.loaded / e.total) * 3), 3)); // Máximo 3 puntos suspensivos
+        let percentage = Math.round((e.loaded / e.total) * 100);
 
-    createOrUpdateAppCacheToast('★ Downloading cache' + dots);
+        createOrUpdateAppCacheToast('Downloading new cache... ' + percentage + '%');
 
-    if (e.loaded + 1 == e.total) {
-        createOrUpdateAppCacheToast("★ Done wait ...");
-    }
-}, false);
+        // the last item takes an unreasonably long time to complete (with a big update)
+        // ig its doing some extra stuff before the last event is fired
+        // so show a new message for it
+        if (e.loaded + 1 == e.total) {
+            createOrUpdateAppCacheToast("Processing... This may take a minute.");
+        }
+    }, false);
 
     appCache.addEventListener('updateready', function (e) {
         if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-            createOrUpdateAppCacheToast('★ Site updated. Refresh');
+            createOrUpdateAppCacheToast('The site was updated. Refresh to switch to updated version');
         }
     }, false);
 }
 
-const TOAST_SUCCESS_TIMEOUT = 2000;
-const TOAST_ERROR_TIMEOUT = 5000;
+function registerL2ButtonHandler() {
+    document.addEventListener("keydown", async (event) => {
+        if (event.keyCode === 118) {
+            const lastRedirectorValue = localStorage.getItem(LOCALSTORE_REDIRECTOR_LAST_URL_KEY) || "http://";
+            const redirectorValue = prompt("Enter url", lastRedirectorValue);
+
+            // pressing cancel works as expected, but pressing the back button unfortunately is the same as pressing ok
+            // so verify that the value is different
+            if (redirectorValue && redirectorValue !== lastRedirectorValue && redirectorValue !== "http://") {
+                localStorage.setItem(LOCALSTORE_REDIRECTOR_LAST_URL_KEY, redirectorValue);
+                window.location.href = redirectorValue;
+            }
+        }
+    });
+}
 
 function showToast(message, timeout = 2000) {
     const toastContainer = document.getElementById('toast-container');
@@ -188,6 +202,7 @@ function showToast(message, timeout = 2000) {
 
     toastContainer.appendChild(toast);
 
+    // Trigger reflow to enable animation
     toast.offsetHeight;
 
     toast.classList.add('show');
@@ -216,66 +231,4 @@ async function removeToast(toast) {
     toast.addEventListener('transitionend', () => {
         toast.remove();
     });
-}
-
-function populatePayloadsPage(wkOnlyMode = false) {
-    const payloadsView = document.getElementById('payloads-view');
-
-    while (payloadsView.firstChild) {
-        payloadsView.removeChild(payloadsView.firstChild);
-    }
-
-const payloads = payload_map;
-
-for (const payload of payloads) {
-    if (wkOnlyMode && !payload.toPort && !payload.customAction) {
-        continue;
-    }
-
-    if (payload.supportedFirmwares && !payload.supportedFirmwares.some(fwPrefix => window.fw_str.startsWith(fwPrefix))) {
-        continue;
-    }
-
-    // Comentamos la creación del botón para que no se vea
-    /*
-    const payloadButton = document.createElement("a");
-    payloadButton.classList.add("btn");
-    payloadButton.classList.add("w-100");
-    payloadButton.tabIndex = 0;
-
-    const payloadTitle = document.createElement("p");
-    payloadTitle.classList.add("payload-btn-title");
-    payloadTitle.textContent = payload.displayTitle;
-
-    const payloadDescription = document.createElement("p");
-    payloadDescription.classList.add("payload-btn-description");
-    payloadDescription.textContent = payload.description;
-
-    const payloadInfo = document.createElement("p");
-    payloadInfo.classList.add("payload-btn-info");
-    payloadInfo.innerHTML = `v${payload.version} &centerdot; ${payload.author}`;
-
-    payloadButton.appendChild(payloadTitle);
-    payloadButton.appendChild(payloadDescription);
-    payloadButton.appendChild(payloadInfo);
-    payloadButton.addEventListener("click", function () {
-        window.dispatchEvent(new CustomEvent(MAINLOOP_EXECUTE_PAYLOAD_REQUEST, { detail: payload }));
-    });
-
-    payloadsView.appendChild(payloadButton);
-    */
-
-    // Crear un nuevo contenedor de mensaje
-    const debugMessage = document.createElement("div");
-    debugMessage.classList.add("btn"); // Usamos las clases btn para el estilo visual, pero no será clickeable
-    debugMessage.style.pointerEvents = "none"; // Deshabilita cualquier interacción
-    debugMessage.style.cursor = "default"; // Elimina el cursor de tipo "mano" para no dar la impresión de que es clickeable
-
-    // El contenido del mensaje que queremos mostrar
-    debugMessage.innerHTML = "★ Debug Settings Ready ✓<br>Waiting payload";
-
-    payloadsView.appendChild(debugMessage); // Agregar el mensaje al contenedor
-}
-
-
 }
